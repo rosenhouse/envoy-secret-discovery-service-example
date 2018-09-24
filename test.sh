a07457f13a9260c2b0f42fdb2505119523bb5e6d
Gabe Rosenhouse <grosenhouse@pivotal.io>
2018-09-23 21:36:04 -0700

    add pilot and ADS

--- test.sh
+++ test.sh
@@ -5,14 +5,22 @@ set -euo pipefail
 docker pull envoyproxy/envoy
 docker pull citizenstig/httpbin
 docker pull byrnedo/alpine-curl
+docker pull istio/pilot:1.0.2
 
 container_subnet="10.255.217.0/24"
 app_container_ip="10.255.217.83"
+pilot_ip="10.255.217.2"
 network_name="envoy-test-net"
 
 # set up custom network, so that diego cert and config works
 docker network rm $network_name || true
 docker network create -d bridge --subnet=$container_subnet $network_name
+
+echo launching pilot
+docker run -d --rm -it --name pilot \
+  --network $network_name --ip $pilot_ip \
+  istio/pilot:1.0.2 \
+    discovery --configDir /dev/null --meshConfig /dev/null --registries Mock
 
 echo launching app, listening on port 8080
 docker run -d --rm -it --name app_container \
@@ -26,7 +34,8 @@ docker run -d --rm -it --name sidecar_proxy \
   -it -v $PWD/envoy_config:/etc/cf-assets/envoy_config \
   envoyproxy/envoy \
     envoy -c /etc/cf-assets/envoy_config/envoy.yaml \
-     --service-cluster proxy-cluster --service-node proxy-node \
+     --v2-config-only \
+     --service-cluster proxy-cluster --service-node "sidecar~$app_container_ip~x~x" \
      --drain-time-s 10 --log-level info
 
 echo waiting for proxy and app to boot
@@ -50,3 +59,4 @@ read -p "Press enter to continue"
 
 docker rm -f sidecar_proxy
 docker rm -f app_container
+docker rm -f pilot
